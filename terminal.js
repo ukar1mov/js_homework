f2472d2b40ea0dc6c9e1a1f842322788c4b23b23
usertheking <ulugbekbekkarimov@gmail.com>
2022-11-14 21:38:39 +0300

    if else exercises

---
+++ terminal.js
@@ -0,0 +1,117 @@
+// 0-task
+
+// 1, if the value is greater than zero,
+// -1, if the value is less than zero,
+// 0, if the value equals zero
+
+
+
+// 0-task is completed
+
+// let a = 5;
+
+// if(a == 0){
+//     console.log(0);
+// }else if(a < 0){
+//     console.log(-1);
+// }else if(a > 0){
+//     console.log(1);
+// }
+
+
+
+
+
+
+// =================first task example of homework
+// let result; 
+
+// if (a + b < 4){
+//     result = 'Below';
+// }else {
+//     result = 'Over';
+// }
+
+
+
+
+
+// =================first task is completed
+// let a = 4;
+//     b = 1;
+
+//     if (a + b < 4){
+//         console.log('Below');
+//     }else {
+//         console.log('Over');
+//     }
+
+
+
+
+
+
+// ===================second task example of the homework
+// let result;
+
+// if (a + b < 4){
+//     result = 'Below';
+// }else{
+//     result = 'Over'
+// }
+
+
+
+
+
+
+
+// =================second tsk is completed
+
+
+// let a = 2;
+//     b = 2;
+
+//     if(a + b < 4){
+//         console.log('Below');
+//     }else{
+//         console.log('Over');
+//     }
+    
+
+
+
+
+
+
+
+
+// ================ if else example of homework
+
+// let message;
+
+// if (login == 'Emplyee'){
+//     message = 'Hello';
+// }else if(login == 'Director'){
+//     message = 'Greetings';
+// }else if(login == ''){
+//     message = 'No login';
+// }else {
+//     message = '';
+// }
+
+
+
+// =============== ternary operators
+
+
+// let login = 'Director';
+
+
+// login == 'Employee'
+// ? console.log('Hello')
+// : login == 'Director'
+// ? console.log('Greetings')
+// : login == ''
+// ? console.log('No login')
+// : console.log('');
